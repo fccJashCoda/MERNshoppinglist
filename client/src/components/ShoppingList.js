c6bc9f17ad2a298e4de32e5a392fcd3a8200f46c
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-01 19:25:56 +0100

    removed: additem button and funcitonalities from ShoppingList

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { v4 as uuid } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
-import { addItem, deleteItem } from '../actions/itemActions';
+import { deleteItem } from '../actions/itemActions';
 
 import ItemModal from './ItemModal';
 
 function ShoppingList(props) {
   const { items } = useSelector((state) => state.item);
   const dispatch = useDispatch();
-
-  const onAddClick = () => {
-    const name = prompt('Enter Item');
-    if (name) {
-      dispatch(addItem({ id: uuid(), name }));
-    }
-  };
 
   const onDeleteClick = (id) => {
     dispatch(deleteItem(id));
@@ -25,13 +17,6 @@ function ShoppingList(props) {
   return (
     <Container>
       <ItemModal />
-      <Button
-        color="dark"
-        style={{ marginBottom: '3rem' }}
-        onClick={onAddClick}
-      >
-        Add Item
-      </Button>
       <ListGroup>
         <TransitionGroup className="shopping-list">
           {items.map(({ id, name }) => (

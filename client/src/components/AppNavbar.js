6729026f560fec3929db7275f666e2bda83117fb
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-05 01:30:29 +0100

    add&fix: user wlecome message ,navbar typo that prevented correct display

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -15,33 +15,48 @@ import { useSelector } from 'react-redux';
 
 function AppNavbar(props) {
   const [isOpen, setIsOpen] = useState(false);
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const guestLinks = (
+    <>
+      <NavItem>
+        <RegisterModal />
+      </NavItem>
+      <NavItem>
+        <LoginModal />
+      </NavItem>
+    </>
+  );
+
+  const authLinks = (
+    <>
+      <NavItem>
+        {user ? (
+          <span className="navbar-text mr-3">
+            Welcome <strong>{user.name}</strong>
+          </span>
+        ) : (
+          ''
+        )}
+      </NavItem>
+      <NavItem>
+        <Logout />
+      </NavItem>
+    </>
+  );
+
   return (
-    <Navbar color="dark" dark expend="sm" className="mb-5">
+    <Navbar color="dark" dark expand="sm" className="mb-5">
       <Container>
         <NavbarBrand href="/">ShoppingList</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
-            {!isAuthenticated ? (
-              <>
-                <NavItem>
-                  <RegisterModal />
-                </NavItem>
-                <NavItem>
-                  <LoginModal />
-                </NavItem>
-              </>
-            ) : (
-              <NavItem>
-                <Logout />
-              </NavItem>
-            )}
+            {isAuthenticated ? authLinks : guestLinks}
           </Nav>
         </Collapse>
       </Container>
